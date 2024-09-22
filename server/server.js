e47f4f5e0f0dd7441feea84974e62ceba3a105c2
Yash Panjwani <yashmpanjwani@gmail.com>
2024-09-22 16:41:08 +0530

    Refactor server.js to use TARGETURL environment variable and update CORS configuration

--- server/server.js
+++ server/server.js
@@ -6,12 +6,15 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 dotenv.config();
 app.use(express.json());
-app.use(cors(
-));
+app.use(cors({
+    origin: ['https://powerupps-ezumin4hq-younglord088s-projects.vercel.app/'], // Add your frontend URL if needed
+    credentials: true,
+  }));
+  
 
 // const proxyUrl = "http://localhost:3002";
 const targetUrl = process.env.TARGETURL;
-
+console.log("targetUrl", targetUrl);
 app.use(
   "/api/auth",
   createProxyMiddleware({
