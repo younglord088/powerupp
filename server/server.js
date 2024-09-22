8822adb83a0f3b59c55569f2c695b132eb987c80
Yash Panjwani <yashmpanjwani@gmail.com>
2024-09-22 15:39:20 +0530

    Add server configuration and dependencies

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");   
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+// const proxyUrl = "http://localhost:3002";
+const targetUrl = "https://www.indiawaterportal.org";
+
+app.use(
+  "/api/auth",
+  createProxyMiddleware({
+    target: targetUrl,
+    changeOrigin: true,
+  })
+);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
